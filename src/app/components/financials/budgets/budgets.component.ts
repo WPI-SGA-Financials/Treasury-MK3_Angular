9ecd40d93fc50a401a6bf01ecc990f255cfed810
Kevin C. Bimonte <kbimonte@gmail.com>
2021-08-27 22:30:25 -0400

    feat: Transitioning Budgets table over to generic table.

--- src/app/components/financials/budgets/budgets.component.ts
+++ src/app/components/financials/budgets/budgets.component.ts
@@ -1,42 +1,66 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../../../services/http.service';
 import { IBudget } from '../../../types/ibudget.interface';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
 import { Path_Api } from '../../../types/path.enum';
+import { ITableColumn } from '../../../types/itable-column.interface';
 
 @Component({
   selector: 'app-budgets',
   templateUrl: './budgets.component.html',
   styleUrls: ['./budgets.component.scss']
 })
-export class BudgetsComponent implements OnInit, AfterViewInit {
-  displayedColumns: string[] = ['nameOfClub', 'fiscalYear', 'numOfItems', 'amountRequested', 'amountProposed', 'amountApproved'];
-  dataSource = new MatTableDataSource<IBudget>([]);
-
-  @ViewChild(MatPaginator) paginator: any;
-  @ViewChild(MatSort) sort: any;
+export class BudgetsComponent implements OnInit {
+  displayedColumns: ITableColumn[] = [];
+  dataSource: IBudget[] = [];
 
   constructor(private http: HttpService) {}
 
   ngOnInit(): void {
+    this.initializeData();
+    this.initializeColumns();
+  }
+
+  private initializeData() {
     this.http.getRequest(Path_Api.BUDGETS).subscribe((response: IBudget[]) => {
       this.setData(response);
     });
   }
 
-  ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+  private setData(data: IBudget[]) {
+    this.dataSource = data;
+  }
+
+  private initializeColumns() {
+    this.displayedColumns = [
+      {
+        name: 'Name of Club',
+        dataKey: 'nameOfClub',
+        isSortable: true
+      },
+      {
+        name: 'Fiscal Year',
+        dataKey: 'fiscalYear'
+      },
+      {
+        name: 'Number of Items',
+        dataKey: 'numOfItems'
+      },
+      {
+        name: 'Amount Requested',
+        dataKey: 'amountRequested'
+      },
+      {
+        name: 'Amount Proposed',
+        dataKey: 'amountProposed'
+      },
+      {
+        name: 'Amount Approved',
+        dataKey: 'amountApproved'
+      }
+    ];
   }
 
   onClickedRow(row: any) {
     console.log(row);
-  }
-
-  private setData(data: IBudget[]) {
-    console.log(data);
-    this.dataSource.data = data;
   }
 }
