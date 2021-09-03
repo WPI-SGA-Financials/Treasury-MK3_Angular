608b0252de3a2093982b37e9cbcf7e683803a861
Kevin C. Bimonte <kbimonte@gmail.com>
2021-09-03 16:40:06 -0400

    feat: create skeleton files for budget popup (tr-22)

---
+++ src/app/components/popups/budget-popup/budget-popup.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-popup',
+  templateUrl: './budget-popup.component.html',
+  styleUrls: ['./budget-popup.component.scss']
+})
+export class BudgetPopupComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/components/popups/budget-popup/budget-popup.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BudgetPopupComponent } from './budget-popup.component';
+
+@NgModule({
+  declarations: [BudgetPopupComponent],
+  imports: [CommonModule]
+})
+export class BudgetPopupModule {}
