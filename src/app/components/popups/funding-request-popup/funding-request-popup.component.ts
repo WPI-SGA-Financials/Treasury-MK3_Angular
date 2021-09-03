9da61255e1062d199a63bc890a43b502c78a88ae
Kevin C. Bimonte <kbimonte@gmail.com>
2021-09-03 16:09:03 -0400

    feat: create skeleton files for funding-request popups (tr-21)

---
+++ src/app/components/popups/funding-request-popup/funding-request-popup.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-funding-request-popup',
+  templateUrl: './funding-request-popup.component.html',
+  styleUrls: ['./funding-request-popup.component.scss']
+})
+export class FundingRequestPopupComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/components/popups/funding-request-popup/funding-request-popup.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FundingRequestPopupComponent } from './funding-request-popup.component';
+
+
+
+@NgModule({
+  declarations: [
+    FundingRequestPopupComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FundingRequestPopupModule { }
