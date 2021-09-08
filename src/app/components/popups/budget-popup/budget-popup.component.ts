207f4a1424284e3d5c1478aab3e6374304a6410f
kcbimonte <kbimonte@gmail.com>
2021-09-08 17:40:08 -0400

    feat: Create Extended Budget Model (tr-19)

--- src/app/components/popups/budget-popup/budget-popup.component.ts
+++ src/app/components/popups/budget-popup/budget-popup.component.ts
@@ -1,4 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpService } from '../../../services/http.service';
+import { Path_Api } from '../../../types/path.enum';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface IExtendedBudget {
+  id: number;
+  nameOfClub: string;
+  fiscalYear: string;
+  numOfItems: number;
+  amountRequested: number;
+  amountProposed: number;
+  approvedAppeal: number;
+  amountApproved: number;
+  amountSpent: number;
+  budgetSections: IBudgetSections[];
+}
+
+export interface IBudgetSections {
+  budgetID: number;
+  nameOfClub: string;
+  fiscalYear: string;
+  sectionName: string;
+  numOfItems: number;
+  amountRequested: number;
+  amountProposed: number;
+  approvedAppeal: number;
+  amountApproved: number;
+  amountSpent: number;
+}
+
+export interface IBudgetDate {
+  id: number
+}
 
 @Component({
   selector: 'app-budget-popup',
