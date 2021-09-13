6518dff865d1c03756d25de2a28a6f9b600620f2
kcbimonte <kbimonte@gmail.com>
2021-09-13 09:00:18 -0400

    feat: create model for extended funding request (tr-18)

--- src/app/components/popups/funding-request-popup/funding-request-popup.component.ts
+++ src/app/components/popups/funding-request-popup/funding-request-popup.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { MatAccordion } from '@angular/material/expansion';
+import { HttpService } from '../../../services/http.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Path, Path_Api } from '../../../types/path.enum';
+import { IFundingRequest } from '../../../types/ifunding-request.interface';
+
+interface IExtendedFR extends IFundingRequest{
+
+}
 
 @Component({
   selector: 'app-funding-request-popup',
