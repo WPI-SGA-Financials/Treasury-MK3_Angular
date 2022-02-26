6681b6bef82a2b515f0d967400be3c7478953cc0
Kevin C. Bimonte <kbimonte@gmail.com>
2022-02-26 11:42:27 -0500

    prox: generate remaining module skeletons for route planning

---
+++ src/app/modules/admin/student-life-fee/student-life-fee.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StudentLifeFeeRoutingModule } from './student-life-fee-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StudentLifeFeeRoutingModule
+  ]
+})
+export class StudentLifeFeeModule { }
